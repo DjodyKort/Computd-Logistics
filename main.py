3a14f1b7cf95ec2e3badeee8762569594b799a16
Djody Kort <507946@vistacollege.nl>
2022-10-12 11:55:55 +0200

    basics

---
+++ main.py
@@ -0,0 +1,9 @@
+# ====== Imports ======
+# Local
+from py.functions import *
+# Packages
+
+# ============ Declaring Variables ============
+
+# ========= Start Program =========
+if __name__ == "__main__":
