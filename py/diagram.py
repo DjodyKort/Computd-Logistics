8499317bd55718ebb41eda5b55c2394716410097
Djody Kort <507946@vistacollege.nl>
2022-11-02 09:14:37 +0100

    d

--- py/diagram.py
+++ py/diagram.py
@@ -1,13 +1,18 @@
 import plotly.express as px
 import pandas as pd
 # Get csv file
-csvDoc = "./csv/OrdersCleanedNew3.csv"
-print(csvDoc, type(csvDoc))
+csvDoc = "./csv/TestData.csv"
 #variables 
 df = pd.read_csv(csvDoc)
 
-fig = px.line(df, x="year", y="lifeExp", color='country')
-fig.show()
 
-fig = px.bar(df, x="Date Placed", y="Time Difference in Days")
+# Find correlation between 'Time Difference in Days' and all the other variables and make a line graph out of it
+corr = df.corr()
+corr = corr['Time Difference in Days']
+corr = corr.drop('Time Difference in Days')
+corr = corr.sort_values(ascending=False)
+print(corr)
+fig = px.line(corr, x=corr.index, y=corr.values)
 fig.show()
+     
+
