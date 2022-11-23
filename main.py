b63a8191e3d818e8812ed5fbb88d719ce9e7b46e
Djody Kort <507946@vistacollege.nl>
2022-11-24 00:32:10 +0100

    big update
    big things ig

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# ======= Imports =======
+# ===================== Imports =====================
+# Packages
+import os
+cmd_output = os.popen('pip install -r requirements.txt').read()
 # Local imports
 from py.functions import *
 
-# Packages
-import os
+
+
 
 # ========== Declaring Variables ==========
 
 # ========= Start Program =========
 if __name__ == "__main__":
-    print("Starting Program")
+    print("Starting Program....")
     # os.system("python ./py/timething.py")
-    os.system("python ./py/csvthings.py")
+    # os.system("python ./py/csvthings.py")
--- py/diagram.py
+++ py/diagram.py
@@ -1,18 +1,46 @@
+# ========= Imports =======
+# Packages
+import numpy as np
 import plotly.express as px
 import pandas as pd
-# Get csv file
-csvDoc = "./csv/TestData.csv"
-#variables 
-df = pd.read_csv(csvDoc)
+import math
+
+# ========== Declaring Variables ==========
+csv_file = './dataframes/csv/OrdersCleanedNew3.csv'
+
+df = pd.read_csv(csv_file)
+new_df = pd.DataFrame(columns = ['City', 'Average Time Difference COD False', 'Average Time Difference COD True', 'Difference', 'Orders COD False', 'Orders COD True','Total Orders', 'Winner'])
+# ========= Start Program =========
+for city in df['City'].unique():
+    boolThingy = True
+    df_city = df[df['City'] == city]
+    df_city = df_city.sort_values(by='Time Difference in Days')
+    df_city = df_city.reset_index(drop=True)
+
+    dfIsCODFalse = df_city[df_city['isCOD'] == False]
+    dfIsCODTrue = df_city[df_city['isCOD'] == True]
+
+    AverageTimeDifferenceCODFalse = round(dfIsCODFalse['Time Difference in Days'].mean(), 3)
+    AverageTimeDifferenceCODTrue = round(dfIsCODTrue['Time Difference in Days'].mean(), 3)
+
+    if math.isnan(AverageTimeDifferenceCODFalse) or math.isnan(AverageTimeDifferenceCODTrue):
+        boolThingy = False
 
 
-# Find correlation between 'Time Difference in Days' and all the other variables and make a line graph out of it
-corr = df.corr()
-corr = corr['Time Difference in Days']
-corr = corr.drop('Time Difference in Days')
-corr = corr.sort_values(ascending=False)
-print(corr)
-fig = px.line(corr, x=corr.index, y=corr.values)
-fig.show()
-     
 
+    if boolThingy:
+        if AverageTimeDifferenceCODFalse < AverageTimeDifferenceCODTrue:
+            difference = round(AverageTimeDifferenceCODTrue - AverageTimeDifferenceCODFalse, 3)
+            winner = 'At the door!'
+        else:
+            difference = round(AverageTimeDifferenceCODFalse - AverageTimeDifferenceCODTrue, 3)
+            winner = 'Online!'
+
+        # Appending to new dataframe
+        new_df = new_df.append({'City': city, 'Average Time Difference COD False': AverageTimeDifferenceCODFalse, 'Average Time Difference COD True': AverageTimeDifferenceCODTrue, 'Difference': difference, 'Orders COD False': len(dfIsCODFalse), 'Orders COD True': len(dfIsCODTrue), 'Total Orders': len(df_city), 'Winner': winner}, ignore_index=True)
+new_df = new_df.sort_values(by='Difference', ascending=False)
+new_df = new_df.reset_index(drop=True)
+
+# Looking if this data actually is important to compute the average time difference per city
+
+new_df.to_excel('./dataframes/excel/TimeDifferenceRenoDing.xlsx', index=False)
